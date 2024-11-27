2b1b758d44561abfbd93d0afb7e85891fd49a736
Wendra <workmailwen@gmail.com>
2024-11-27 12:20:58 +0700

    Initial Commit

---
+++ EbuddyTest/EbuddyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EbuddyTestApp.swift
+//  EbuddyTest
+//
+//  Created by Wendra on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EbuddyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
